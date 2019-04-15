7985dd32a88ac97bcb89c4d292c2182101f0dd9a
Taylor <taylor.nodell@gmail.com>
2019-04-15 12:19:08 -0400

    init commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import Phaser from "phaser";
+import logoImg from "./assets/logo.png";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.jsx";
+
+console.log(App);
+
+const config = {
+  type: Phaser.AUTO,
+  parent: "phaser-example",
+  width: 800,
+  height: 600,
+  scene: {
+    preload: preload,
+    create: create
+  }
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+  this.load.image("logo", logoImg);
+}
+
+function create() {
+  const logo = this.add.image(400, 150, "logo");
+
+  this.tweens.add({
+    targets: logo,
+    y: 450,
+    duration: 2000,
+    ease: "Power2",
+    yoyo: true,
+    loop: -1
+  });
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
